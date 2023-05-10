b8bcd8ba4b4a3c90b7c14021b337fbbe3d3109b0
omar chatin <omerchatin19@gmail.com>
2023-05-10 09:53:18 +0300

    d

--- app/(menu)/[restaurantId]/(layoutgroup)/page.tsx
+++ app/(menu)/[restaurantId]/(layoutgroup)/page.tsx
@@ -22,9 +22,6 @@ type Props = {
 };
 
 export default async function page({ params, searchParams }: Props) {
-  const categoriesRes =
-    await `${baseUrl}/api/restaurants/${params.restaurantId}/categories`;
-  const categories = await categoriesRes.json();
   return (
     <div>
       {/* @ts-expect-error - Server Component */}
